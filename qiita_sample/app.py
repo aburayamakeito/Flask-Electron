bb98d862d624dc77f933157df5398023749f0b2c
Keito <abu6101k@gmail.com>
2022-07-10 19:26:20 +0900

    構成の見直し

--- qiita_sample/app.py
+++ qiita_sample/app.py
@@ -1,6 +1,16 @@
+import os
 from flask import Flask, render_template
 
 app = Flask(__name__, instance_relative_config=True)
+
+# 標準設定ファイル読み込み
+app.config.from_object("settings")
+
+# 非公開設定ファイル読み込み
+if app.config["ENV"] == "development":
+    app.config.from_pyfile(os.path.join("config", "development.py"), silent=True)
+else:
+    app.config.from_pyfile(os.path.join("config", "production.py"), silent=True)
 
 @app.route("/", methods=("GET", "POST"))
 def index():
